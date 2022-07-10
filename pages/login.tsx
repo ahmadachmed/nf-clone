acf32a89b9f7a3a999d3a5e6504e5540984a7c5b
Ahmad Ilham <ahmad.ilham@amartha.com>
2022-07-11 00:01:56 +0700

    first commit

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,7 +9,7 @@ interface Inputs {
   password: string;
 }
 
-function login() {
+function Login() {
   const [login, setLogin] = useState(false);
   const {signIn, signUp} = useAuth()
 
@@ -90,4 +90,4 @@ function login() {
   );
 }
 
-export default login;
+export default Login;
